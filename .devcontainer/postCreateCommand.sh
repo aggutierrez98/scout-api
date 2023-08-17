9e8b79d0bcb418f33170b4be1325f82f616f69f9
Agustin Gutierrez <agustinguti123@gmail.com>
2023-08-17 18:48:36 -0300

    Add some modifications to devcontainers configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,5 @@
 apt-get update && apt-get install zsh -y
-yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# apt-get install openssh-client -y
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+zsh
 # clear && zsh
