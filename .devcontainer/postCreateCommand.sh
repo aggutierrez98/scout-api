e9a778c2e6b0d30944a7b0ebaca9bea65d064d4a
Agustin Gutierrez <agustinguti123@gmail.com>
2023-08-19 14:08:54 +0000

    Change postCreateCommand to postAttachCommand in devontainer.json

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get install zsh -y
-# apt-get install openssh-client -y
+apt-get install openssh-client -y
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 zsh
 # clear && zsh
