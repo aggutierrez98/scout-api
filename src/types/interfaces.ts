3f828b892492b73bbba5694324174874852ddc40
Agustin Gutierrez <agustinguti123@gmail.com>
2023-09-22 16:34:00 -0300

    Refactor types and interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -2,30 +2,11 @@ import { ProgresionEnum, TipoInsigniaEnum } from "./XLSXTypes";
 import {
 	FuncionType,
 	ProgresionType,
+	RelacionFamiliarType,
 	ReligionType,
 	SexoType,
+	TipoInsigniaType,
 } from "./constantTypes";
-
-export interface IFamiliarScout {
-	id: number;
-	familiarId: number;
-	scoutId: number;
-}
-
-export interface IDocPresentado {
-	id: number;
-	documentoId: number;
-	scoutId: number;
-	fechaPresentacion: Date;
-}
-
-export interface IInsignaObt {
-	id: number;
-	scoutId: number;
-	insignia: TipoInsigniaEnum;
-	progresion: ProgresionEnum;
-	fechaObtencion: Date;
-}
 
 export interface IScout {
 	nombre: string;
@@ -40,14 +21,83 @@ export interface IScout {
 	patrullaId: number | null;
 	religion: ReligionType | null;
 	progresionActual: ProgresionType | null;
-	Funcion: FuncionType | null;
+	funcion: FuncionType | null;
 }
 
 export interface IScoutData extends IScout {
 	id: number;
-	documentosPresentados?: IDocPresentado[];
-	familiarScout?: IFamiliarScout[];
-	insigniasObtenidas?: IInsignaObt[];
+	documentosPresentados?: {
+		id: number;
+		documento: {
+			nombre: string;
+			vence: boolean;
+		};
+		fechaPresentacion: Date;
+	}[];
+	insigniasObtenidas?: {
+		id: number;
+		insignia: TipoInsigniaType;
+		progresion: ProgresionType | null;
+		fechaObtencion: Date;
+	}[];
+	familiarScout?: {
+		relacion: RelacionFamiliarType;
+		familiar: {
+			id: number;
+			nombre: string;
+			apellido: string;
+			dni: string;
+			telefono: string;
+			sexo: SexoType;
+			fechaNacimiento: Date;
+		};
+	}[];
 	fechaActualizacion: Date;
 	fechaCreacion: Date;
+}
+
+export interface IPatrulla {
+	nombre: string;
+	lema?: string | null;
+	scouts?: IScout[];
+}
+
+export interface IPatrullaData extends IPatrulla {
+	id: number;
+	fechaCreacion: Date;
+	fechaActualizacion: Date;
+}
+
+export interface IFamiliarScout {
+	id: number;
+	familiarId: number;
+	scouts: IScout[];
+}
+
+export interface IInsignaObt {
+	id: number;
+	scoutId: number;
+	insignia: TipoInsigniaType;
+	progresion: ProgresionType | null;
+	fechaObtencion: Date;
+}
+
+export interface IDocumento {
+	scoutId: string;
+	documentoId: string;
+	fechaPresentacion: Date;
+}
+
+export interface IDocumentoData {
+	id: number;
+	scout: {
+		nombre: string;
+		apellido: string;
+	};
+	documento: {
+		nombre: string;
+		vence: boolean;
+	};
+	scoutId: number;
+	fechaPresentacion: Date;
 }
